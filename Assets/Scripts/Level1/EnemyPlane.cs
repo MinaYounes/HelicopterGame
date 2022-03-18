1497a8ce50918a1e0d0878cf5d57fb9f6f50087c
Mina Younes <88724169+MinaYounes@users.noreply.github.com>
2022-03-18 14:42:22 -0400

    Enemy planes added to game
    Planes move with a speed

---
+++ Assets/Scripts/Level1/EnemyPlane.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPlane : MonoBehaviour
+{
+
+    [HideInInspector]
+    public float speed;
+    private Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        // Enemy speed
+        speed = 3f;
+    }
+
+    void FixedUpdate()
+    {
+        // Makes enemy planes move on X axis while staying on same Y point
+        rb.velocity = new Vector2(speed, rb.velocity.y);
+    }
+}

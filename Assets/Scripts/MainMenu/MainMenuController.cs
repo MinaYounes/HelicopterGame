a473409a187480787216cade427da8db738b4a9e
Mina Younes <88724169+MinaYounes@users.noreply.github.com>
2022-04-08 17:24:28 -0400

    message displays when email entered on main menu

--- Assets/Scripts/MainMenu/MainMenuController.cs
+++ Assets/Scripts/MainMenu/MainMenuController.cs
@@ -2,12 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
+    private Text bonusCoins;
+
+    private void Update()
+    {
+        // if the email is entered or was entered, "+100 coins" continues to display on main menu
+        if(ReadInput.emailEntered == true)
+        {
+            bonusCoins = GameObject.Find("ThanksForEmail").GetComponent<Text>();
+            bonusCoins.text = "+100 coins!";
+        }
+    }
+
     // clicking new game goes to level 1 game scene
     public void PlayGame()
     {
+        // reset game components
         EnemyPlane.level = 1;
 
         Bullet.speed = 6f;
@@ -19,10 +33,12 @@ public class MainMenuController : MonoBehaviour
         Bullet3.speed = 8f;
         Bullet3.damage = 10;
 
+        // if email is set to true, always give 100 bonus coins when game restarts
         if (ReadInput.emailEntered == true)
         {
             ShopController.coins += 100;
         }
+        // if email not given, no bonus coins
 	    else
         {
             ShopController.coins = 0;
--- Assets/Scripts/MainMenu/ReadInput.cs
+++ Assets/Scripts/MainMenu/ReadInput.cs
@@ -1,28 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ReadInput : MonoBehaviour
 {
     private string input;
     public static bool emailEntered = false;
- 
-    void Start()
-    {
   
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     // take user email input
     public void ReadString(string text)
     {
         input = text;
-        emailEntered = true;
-        Debug.Log("email: " + input);
+        emailEntered = true;   
     }
 }
